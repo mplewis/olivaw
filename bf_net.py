a4875ce13daaca01fa7a712c0738be73bcfd9f36
Patrick Severin <pbseverin@gmail.com>
2014-10-26 20:14:21 -0500

    Network now passes commands

--- bf_net.py
+++ bf_net.py
@@ -9,10 +9,14 @@ class BFBot(Bot):
 	def tick(self):
 		select = randint(1,20)
 		if select == 1:
-			address = self.peers[randint(0,len(self.peers)-1)]
-			print("This is {} pinging {}".format(self.id, address))
-			net.send(self.id, address, "ping")
+			#address = self.peers[randint(0,len(self.peers)-1)]
+			#print("This is {} pinging {}".format(self.id, address))
+			#net.send(self.id, address, "ping")
 			return
+		if select == 2:
+			self.getPeer()
+		if select > 18:
+			self.getCommand()
 
 	def getPeer(self):
 		#select random peer
@@ -20,27 +24,49 @@ class BFBot(Bot):
 		#ask for one new peer
 		net.send(self.id, address, "getPeer")
 
+	def getCommand(self):
+		#select random peer
+		address = self.peers[randint(0,len(self.peers)-1)]
+		#ask for its current command
+		net.send(self.id, address, "getCommand")
+
 	def recieve(self, srcID, message):
 		if message == "ping":
 			print('Hi {}, this is {}.'.format(srcID, self.id))
 			return
 		if message == "getPeer":
-			net.send(self.id, srcID, "getPeerResponse:"+self.peers[randint(0,len(self.peers)-1)])
+			net.send(self.id, srcID,
+			 "getPeerResponse:{}".format(self.peers[randint(0,len(self.peers)-1)]))
 			return
 		if "getPeerResponse" in message:
 			#parse out address
-			recievedPeer = message[message.index(":")+1:]
+			recievedPeer = int(message[message.index(":")+1:])
 			#check peer list for duplication
 			for peer in self.peers:
 				if peer == recievedPeer:
 					return
 			#select random peer to replace
 			self.peers[randint(0,len(self.peers)-1)] = recievedPeer
+		if message == "getCommand":
+			net.send(self.id, srcID, 
+				"getCommandResponse:{},{}".format(self.command[0], self.command[1]))
+			return
+		if "getCommandResponse" in message:
+			commandNumber = int(message[message.index(":")+1:message.index(",")])
+			if commandNumber > self.command[0]:
+				self.command = (commandNumber, message[message.index(",")+1:])
+			return
 
 class TestNet(Net):
     def tick(self):
-        print('Tick %s' % (self.ticks + 1))
+        #print('Tick %s' % (self.ticks + 1))
         super().tick()
+    def dumpbots(self):
+    	for bot in self.bots:
+    		print("{}({}) : {}".format(bot.id, bot.command, bot.peers))
+    	print("---------------------------")
+    def command(self):
+    	print("New command sent to {}.".format(super().command()))
 
 
 if __name__ == '__main__':
@@ -56,5 +82,12 @@ if __name__ == '__main__':
     	for x in range(5):
     		bot.peers.append(botids[randint(0, len(botids)-1)])
     net.bots = bots
-    for x in range(100):
-        net.tick()
+    net.dumpbots()
+    net.command()
+    for x in range(10):
+	    for x in range(50):
+	        net.tick()
+	    net.command()
+	    net.dumpbots()
+	    
+	    
--- prototypes.py
+++ prototypes.py
@@ -4,6 +4,7 @@ from random import randint
 class Net:
     def __init__(self):
         self.ticks = 0
+        self.commandNumber = 1
         self.bots = []
 
     def tick(self):
@@ -16,6 +17,14 @@ class Net:
             if bot.id == destID:
                 bot.recieve(srcID, msg)
 
+    def command(self):
+        #send new command to a random bot
+        bot = self.bots[randint(0, len(self.bots)-1)]
+        bot.command = (self.commandNumber, "bitmine@{}".format(self.ticks))
+        self.commandNumber+=1
+        return bot.id
+
+
 
 class Bot:
     # net === the common Net object between all bots
@@ -24,6 +33,7 @@ class Bot:
         self.peers = []
         self.id = randint(10000, 99999)
         self.net = net
+        self.command = (0, "v.0")
     def tick(self):
         err = ('Bot must implement a tick() method indicating what happens '
                'each timestep.')
