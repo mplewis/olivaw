2f1cd62c0dc9caf87443d7c0542208971f893d65
Matthew Lewis <matt@mplewis.com>
2014-12-02 01:34:46 -0600

    Reformat all code

--- Java/Olivaw/src/CrawlBot.java
+++ Java/Olivaw/src/CrawlBot.java
@@ -3,21 +3,21 @@ import java.util.*;
 public class CrawlBot extends GoodBot {
 
     private Set<ZeroAccessBot> discoveredBots = new HashSet<ZeroAccessBot>();
-    
+
     @Override
     public void tick() {
         private Set<ZeroAccessBot> newBots = new HashSet<ZeroAccessBots>();
-        
-        for ( ZeroAccessBot bot : discoveredBots ) {
+
+        for (ZeroAccessBot bot : discoveredBots) {
             List<ZeroAccessBot> peers = bot.knownPeers(this);
-            for ( ZeroAccessBot peer : peers ) {
-                if ( !discoveredBots.contains(peer) ) {
+            for (ZeroAccessBot peer : peers) {
+                if (!discoveredBots.contains(peer)) {
                     newBots.add(peer);
                 }
             }
         }
         discoveredBots.addAll(newBots);
-        
+
         super.tick();
     }
 }
--- Java/Olivaw/src/ZeroAccessBot.java
+++ Java/Olivaw/src/ZeroAccessBot.java
@@ -9,9 +9,13 @@ import java.util.List;
 public interface ZeroAccessBot {
 
     public List<ZeroAccessBot> knownPeers(ZeroAccessBot caller);
+
     public void tick();
+
     public void setVersion(int version);
+
     public int getVersion();
+
     public void setPeers(Deque<ZeroAccessBot> peers);
 
 }
