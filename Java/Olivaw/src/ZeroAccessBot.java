b87d1af4c3e14c6fa9ee29f5cf8fe4f89cd50c13
Matthew Lewis <matt@mplewis.com>
2014-12-02 00:55:12 -0600

    More implementations of methods ported to Java

--- Java/Olivaw/src/ZeroAccessBot.java
+++ Java/Olivaw/src/ZeroAccessBot.java
@@ -1,9 +1,12 @@
+import java.util.Deque;
 import java.util.List;
 
 public interface ZeroAccessBot {
 
     public List<ZeroAccessBot> knownPeers();
     public void tick();
+    public void setVersion(int version);
     public int getVersion();
+    public void setPeers(Deque<ZeroAccessBot> peers);
 
 }
