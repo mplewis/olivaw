4537d6c6cb870ef532149e4501a161b990aa0471
Matthew Lewis <matt@mplewis.com>
2014-12-02 00:01:48 -0600

    Add bot interface and good bot implementation

---
+++ Java/Olivaw/src/GoodBot.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class GoodBot implements ZeroAccessBot {
+
+    private static final int KNOWN_PEER_COUNT = 256;
+    private static final int PEERS_TO_RETURN = 16;
+    private static final Random rng = new Random();
+
+    private final Deque<ZeroAccessBot> peers = new LinkedList<ZeroAccessBot>();
+
+    private int version;
+
+    public GoodBot() {
+        version = 0;
+    }
+
+    @Override
+    public List<ZeroAccessBot> knownPeers() {
+        Set<ZeroAccessBot> selectedPeers = new HashSet<ZeroAccessBot>();
+        while (selectedPeers.size() < PEERS_TO_RETURN) {
+            int index = rng.nextInt(KNOWN_PEER_COUNT);
+            LinkedList<ZeroAccessBot> peersList = (LinkedList<ZeroAccessBot>) peers;
+            ZeroAccessBot peer = peersList.get(index);
+            selectedPeers.add(peer);
+        }
+        return new ArrayList<ZeroAccessBot>(selectedPeers);
+    }
+
+    @Override
+    public void tick() {
+        LinkedList<ZeroAccessBot> peersList = (LinkedList<ZeroAccessBot>) peers;
+        int index = rng.nextInt(KNOWN_PEER_COUNT);
+        ZeroAccessBot peer = peersList.get(index);
+        List<ZeroAccessBot> rcvdPeers = peer.knownPeers();
+        for (ZeroAccessBot newPeer : rcvdPeers) {
+            peers.remove();
+            peers.add(newPeer);
+        }
+    }
+
+    public int getVersion() {
+        return version;
+    }
+}
---
+++ Java/Olivaw/src/ZeroAccessBot.java
@@ -0,0 +1,8 @@
+import java.util.List;
+
+public interface ZeroAccessBot {
+
+    public List<ZeroAccessBot> knownPeers();
+    public void tick();
+
+}
