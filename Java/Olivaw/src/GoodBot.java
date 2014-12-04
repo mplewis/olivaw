3ec57162e9caf0e03af91488b7e09290e317b971
Kevin Viratyosin <virat001@umn.edu>
2014-12-04 00:10:00 -0600

    Fixed GoodBot - no longer accept duplicate peers

--- Java/Olivaw/src/GoodBot.java
+++ Java/Olivaw/src/GoodBot.java
@@ -54,6 +54,7 @@ public class GoodBot implements ZeroAccessBot {
 
         // Add its known peers to own peer list, replacing existing peers
         List<ZeroAccessBot> receivedPeers = peer.knownPeers(this);
+        receivedPeers.removeAll(this.peers);
         for (ZeroAccessBot newPeer : receivedPeers) {
             peers.add(newPeer);
         }
